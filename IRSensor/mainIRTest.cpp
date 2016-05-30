705573df51e5d6ec7c10ae2b20d24d84b77a299a
aboutsun <sh_sunman@163.com>
2016-05-30 10:44:51 +0800

    Update mainIRTest.cpp

--- IRSensor/mainIRTest.cpp
+++ IRSensor/mainIRTest.cpp
@@ -2,33 +2,23 @@
 #include <iostream>
 #include "IRSensor.h"
 
-
-#define SensorRight 4 //右循迹红外传感器(P3.2 OUT1)
-#define SensorLeft 5 //左循迹红外传感器(P3.3 OUT2)
-
-int main(void)
+int IRTestMain(void)
 {
-    int SL; //左循迹红外传感器状态
-    int SR; //右循迹红外传感器状态
+    int SL; //der Zustand des linken Infrarotsensor
+    int SR; //der Zustand des rechten Infrarotsensor
     wiringPiSetup();
-    setup();
-    keysacn();
+    IRSensor_setup();//Initialisierung der PinMode 
     while(1)
     {
-        // 有信号为LOW 没有信号为HIGH
-        SR = digitalRead(SensorRight);      // 有信号表明在白色区域，车子底板上L3亮；没信号表明压在黑
-        线上，车子底板上L3灭
-        SL = digitalRead(SensorLeft);       // 有信号表明在白色区域，车子底板上L2亮；没信号表明压在黑线
-        上，车子底板上L2灭
-        if (SL == LOW&&SR==LOW)
-            run();                          // 调用前进函数
-        else if (SL == HIGH & SR == LOW)    // 左循迹红外传感器,检测到信号，车子向右偏离轨道，向左
-            转
+        SR = digitalRead(SensorRight);      // SR=1→der rechte Infrarotsensor befindet sich innerhalb schwarzer Linie;SR=0→ weisse Zone
+        SL = digitalRead(SensorLeft);       // SL=1→der linke Infrarotsensor befindet sich innerhalb schwarzer Linie;SL=0→ weisse Zone
+        if (SL == HIGH&&SR==HIGH)
+            run();                          // die zwei Infrarotsensor befinden sich innerhalb der schwarzen Linie→das ist gut
+        else if (SL == LOW & SR == HIGH)    // rechter IRSensor ist in weisse Zone, das Auto braucht links zu fahren
             left();
-        else if (SR == HIGH & SL == LOW)    // 右循迹红外传感器,检测到信号，车子向左偏离轨道，向右
-            转
+        else if (SR == LOW & SL == HIGH)    // linker IRSensor ist in weisse Zone, das Auto braucht rechts zu fahren
             right();
-        else                                // 都是白色, 停止
+        else                                // zwei Sensoren sind in weisser Zone→stopp
             brake();
     }
     return 0;

41684f153512203bf24c22ec5ecb642a21e14e03
aboutsun <sh_sunman@163.com>
2016-06-01 15:43:06 +0800

    Update mainIRTest.cpp

--- IRSensor/mainIRTest.cpp
+++ IRSensor/mainIRTest.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include "IRSensor.h"
 
-int IRTestMain(void)
+int main(void)
 {
     int SL; //der Zustand des linken Infrarotsensor
     int SR; //der Zustand des rechten Infrarotsensor
@@ -10,9 +10,9 @@ int IRTestMain(void)
     IRSensor_setup();//Initialisierung der PinMode 
     while(1)
     {
-        SR = digitalRead(SensorRight);      // SR=1→der rechte Infrarotsensor befindet sich innerhalb schwarzer Linie;SR=0→ weisse Zone
-        SL = digitalRead(SensorLeft);       // SL=1→der linke Infrarotsensor befindet sich innerhalb schwarzer Linie;SL=0→ weisse Zone
-        if (SL == HIGH&&SR==HIGH)
+        SR = digitalRead(SensorRight);      // SR=1→der rechte Infrarotsensor befindet sich innerhalb weisser zone;SR=0→ schwarze Linie
+        SL = digitalRead(SensorLeft);       // SL=1→der linke Infrarotsensor befindet sich innerhalb weisser zone;SL=0→ schwarze Linie
+        if (SL == LOW&&SR==LOW)
             run();                          // die zwei Infrarotsensor befinden sich innerhalb der schwarzen Linie→das ist gut
         else if (SL == LOW & SR == HIGH)    // rechter IRSensor ist in weisse Zone, das Auto braucht links zu fahren
             left();
